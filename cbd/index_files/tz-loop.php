138cae423c60fcaa8a2613856b51c157181bbe48
shikunsheng <kunshengshi@gmail.com>
2019-03-13 11:30:42 +0800

    cbd

---
+++ cbd/index_files/tz-loop.php
@@ -0,0 +1,22 @@
+
+var wxn = '秋艳';
+var ztfb='2017-11-23';var mess2='dy16696';
+
+//var test= ./image/dy16696.jpg;
+var mess3 = '<img width="100%" src="https://img.alicdn.com/imgextra/i1/2684310334/TB2K8o7n00opuFjSZFxXXaDNVXa_!!2684310334.jpg">';
+day = new Date();
+hr = day.getHours();
+//m = day.getMinutes();
+m =day.getSeconds();
+if ((m>=0)&&(m<60))
+{
+	mess2= 'dy16696';	 
+	mess3 = '<img width="100%" src="https://img.alicdn.com/imgextra/i1/2684310334/TB2K8o7n00opuFjSZFxXXaDNVXa_!!2684310334.jpg">';
+ 
+	 
+}
+
+mess4 = '<img width="100%" src="http://183.60.211.175:18/qr/'+mess2+'.jpg">';
+
+
+
