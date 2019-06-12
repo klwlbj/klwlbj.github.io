29866d8a46a020903dd5c461b281df81f8692064
shikunsheng <kunshengshi@gmail.com>
2019-06-12 23:21:22 +0800

    f

---
+++ 1/static/js/weixin.js
@@ -0,0 +1,46 @@
+var rand = (function () {
+	var today = new Date();
+	var seed = today.getTime();
+
+	function rnd() {
+		seed = (seed * 9301 + 49297) % 233280;
+		return seed / (233280.0)
+	};
+	return function rand(number) {
+		return Math.ceil(rnd(seed) * number)
+	}
+})();
+
+var arr_wx = ['855-885609511','18417143458','17126641316','855-885585697'];
+
+var wxindex = getCookie("wx_index");
+
+if (wxindex != "") {
+	var wx_index = wxindex;
+} else {
+	var wx_index = rand(arr_wx.length) - 1;
+	setCookie("wx_index", wx_index, 5);
+}
+
+var stxlwx = arr_wx[wx_index];
+
+//var img =  arr_wx[wx_index];
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 60 * 1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i].trim();
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
